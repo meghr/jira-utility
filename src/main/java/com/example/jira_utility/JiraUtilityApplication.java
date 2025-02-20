4b54762028144980a6532c49a38a0c62e6951133
Pooja Sharma <pooja.meghrajsharma@gmail.com>
2025-02-21 00:57:52 +0530

    Initial commit

---
+++ src/main/java/com/example/jira_utility/JiraUtilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.jira_utility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraUtilityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jira_utility/JiraUtilityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jira_utility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraUtilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
